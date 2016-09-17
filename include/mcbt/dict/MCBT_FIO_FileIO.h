054c9b4d2a3cd60e66d7ff8f6006e4c253f9d1e3
VVind0wM4ker <VVind0wM4ker@users.noreply.github.com>
2016-09-17 22:32:29 +0200

    Update MCBT_FIO_FileIO.h
    formatting

--- include/mcbt/dict/MCBT_FIO_FileIO.h
+++ include/mcbt/dict/MCBT_FIO_FileIO.h
@@ -12,30 +12,31 @@ public:
 	{}
 	~BTFile()
 	{}
-			bool	validateContent(void) const;
+
+		bool	validateContent(void) const;
 	static	bool	validateContent(const std::string &sContent);
 
-			bool	standardise(void);
+		bool	standardise(void);
 	static	bool	standardise(std::string &sContent);
 
-			void	readFile(const bool validate = true);
+		void	readFile(const bool validate = true);
 	static	void	readFile(const std::string sFilePath, std::string &sContent, const bool validate = true);
 
-			void	writeFile(const bool validate = true) const;
-			void	writeFile(const std::string sFilePath, const bool validate = true) const;
+		void	writeFile(const bool validate = true) const;
+		void	writeFile(const std::string sFilePath, const bool validate = true) const;
 	static	void	writeFile(const std::string sFilePath, const std::string &sContent, const bool validate = true);
 
-			void	deleteFile(void);
+		void	deleteFile(void);
 	static	void	deleteFile(const std::string sFilePath);
 
-			void	getContent(std::string &sContent, const bool validate = true) const;
-			void	setContent(const std::string &sContent, const bool validate = true);
+		void	getContent(std::string &sContent, const bool validate = true) const;
+		void	setContent(const std::string &sContent, const bool validate = true);
 
 private:
 	BTFile();
 
-					std::string sContent;
-					std::string sFilePath;
+			std::string sContent;
+			std::string sFilePath;
 
 	static	const	std::regex re_basic_structure;
 	static	const	std::regex re_structure_fragment;
